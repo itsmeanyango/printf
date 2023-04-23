aee68e50a9435fb38dd352c93e7d81f8822430d0
abdulmaleekadams <harbdulmaleek@gmail.com>
2023-04-23 16:12:24 +0100

    Check error

--- _printf.c
+++ _printf.c
@@ -14,7 +14,7 @@ int _printf(const char *format, ...)
 	va_list args;
 
 	va_start(args, format);
-	if (format == NULL)
+	if (format == NULL || format[0] == '\0')
 		return (-1);
 
 	while (format && *format != '\0')
